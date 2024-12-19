d7b5647fc96c723131e8eb8d864061c96e23312a
ariefr29 <64705049+ariefr29@users.noreply.github.com>
2024-12-19 21:53:19 +0700

    Update Header.js

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,7 +6,7 @@ function Header(props) {
       <div className="flex-container md-flex-column responsive-text-center">
         <div className="text md-order-2">
           <h1 className="putih text-bold">Halo, Saya <span className="oren">{props.name}</span></h1>
-          <p>Salam kenal, saya hanyalah anak laki-laki yang lahir di generasi akhir 90an yang pernah berpikir bahwa dunia itu sederharna, dan ternyata itu lebih sederhana daripada yang dipikirkan. </p>
+          <p>Salam kenal, saya senang berpikir, membaca, mengamati, dan menulis. Terkadang saya suka membual berbicara omong kosong dan bahkan menikmatinya... Hahaha. </p>
           <a href={props.urlButton}><button className="btn">Contact Me</button></a>
         </div>
         <div className="avatar lh-0 border-radius oren">
